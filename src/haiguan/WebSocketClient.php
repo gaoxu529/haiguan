953b267d0616918eb361ce42604a462a75c0e290
青苹果 <gaoxu529@126.com>
2019-05-29 14:24:06 +0800

    修改websocketclient

--- src/haiguan/WebSocketClient.php
+++ src/haiguan/WebSocketClient.php
@@ -59,7 +59,7 @@ class WebSocketClient
     static public function getInstance($socket_uri)
     {//声明一个getinstance()静态方法，用于检测是否有实例对象
         if (!self::$instance)
-            self::$instance = new ClientService($socket_uri);
+            self::$instance = new WebSocketClient($socket_uri);
         return self::$instance;
     }
 
